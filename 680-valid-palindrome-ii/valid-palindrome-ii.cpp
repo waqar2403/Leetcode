d1cc494f1b5eb6a8eb86bad1891edf6e58fc4810
Muhammad Waqar <140682775+waqar2403@users.noreply.github.com>
2024-10-19 22:17:50 +0500

    Time: 3 ms (100.00%) | Memory: 21.3 MB (70.69%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -16,13 +16,12 @@ public:
         
         while (left < right) {
             if (s[left] != s[right]) {
-                // Try skipping the left or the right character
                 return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
             }
             left++;
             right--;
         }
         
-        return true;  // If no mismatch is found, it's already a palindrome
+        return true;  
     }
 };
