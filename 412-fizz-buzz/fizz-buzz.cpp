22438b1608d0465ea8014e3aa36e30787a054b83
Muhammad Waqar <140682775+waqar2403@users.noreply.github.com>
2025-01-10 21:34:36 +0500

    Time: 0 ms (100.00%) | Memory: 11.1 MB (83.59%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> arr(n);
+        arr[0] = "1";
+        for(int i=1;i<=n;i++)
+        {
+            if(i%3==0 && i%5==0)
+            {
+                arr[i-1] = "FizzBuzz";
+            }
+            else if (i%3==0)
+            {
+                arr[i-1] = "Fizz";
+            }
+            else if(i%5==0)
+            {
+                arr[i-1] = "Buzz";
+            }
+            else
+            {
+                arr[i-1]= to_string(i);
+            }
+
+        }
+        return arr;
+    }
+};
