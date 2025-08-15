1a93f398641d1ef251738b7a168a07a4797cdbfa
Muhammad Waqar <140682775+waqar2403@users.noreply.github.com>
2025-08-14 23:27:59 -0700

    Time: 1 ms (8.57%) | Memory: 11.1 MB (5.34%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-        int a,b;
-        a = 0;b=0;
-        while(a<s.length() && b<t.length()){
-            if(s[a] == t[b])
-            {
-                a++;b++;
-            }
-            else
-            {
-                b++;
-            }
-        }
-        if(a == s.length())
-        {
+
+    int isSubs(string& s1, string& s2, int i , int j,vector<vector<int>> &t)
+    {
+        if(i == 0 || j == 0)
+            return 0;
+        if(t[i][j] != -1)
+            return t[i][j];
+        if(s1[i-1] == s2[j-1])
+            return t[i][j] = 1 + isSubs(s1,s2,i-1,j-1,t);
+        else
+            return t[i][j] = isSubs(s1,s2,i,j-1,t);
+    }
+    
+    
+    bool isSubsequence(string s1, string s2) {
+        int m = s1.size();
+        int n = s2.size();
+        
+        if(m >  n)
+            return false;
+        
+        vector<vector<int>> t(m+1,vector<int> (n+1,-1));
+    
+        if(isSubs(s1,s2,m,n,t) == m)
             return true;
-        }
         return false;
     }
 };
