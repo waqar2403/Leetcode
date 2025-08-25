6732f42e099cbd85269a251f579bdcd046aa7a0e
Muhammad Waqar <140682775+waqar2403@users.noreply.github.com>
2025-08-25 14:08:30 +0500

    Time: 4 ms (48.21%) | Memory: 18.4 MB (66.30%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+struct cmp
+{
+    bool operator()(ListNode* x, ListNode* y) const
+    {
+        return x->val > y->val;
+    }
+};
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, cmp> pq;
+        for (auto node : lists) if (node) pq.push(node);
+
+        ListNode dummy(0);
+        ListNode *tail = &dummy;
+
+        while (!pq.empty())
+        {
+            ListNode* node = pq.top(); pq.pop();
+            tail->next = node;
+            tail = tail->next;
+            if (node->next) pq.push(node->next);
+        }
+
+        return dummy.next;
+    }
+};
