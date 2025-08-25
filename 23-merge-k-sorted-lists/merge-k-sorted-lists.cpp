abc4b2aba8ba8eac16636e0ca74e3ef5bfdc5b6c
Muhammad Waqar <140682775+waqar2403@users.noreply.github.com>
2025-08-25 15:10:36 +0500

    Time: 7 ms (33.15%) | Memory: 18.3 MB (81.82%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -8,25 +8,31 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+struct cmp
+{
+    bool operator()(ListNode* x, ListNode* y) const
+    {
+        return x->val > y->val;
+    }
+};
 
-class Solution {    
+class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int> nodes;
-        for (ListNode* lst : lists) {
-            while (lst) {
-                nodes.push_back(lst->val);
-                lst = lst->next;
-            }
-        }
-        sort(nodes.begin(), nodes.end());
+        priority_queue<ListNode*, vector<ListNode*>, cmp> pq;
+        for (auto node : lists) if (node) pq.push(node);
 
-        ListNode* res = new ListNode(0);
-        ListNode* cur = res;
-        for (int node : nodes) {
-            cur->next = new ListNode(node);
-            cur = cur->next;
+        ListNode dummy(0);
+        ListNode *tail = &dummy;
+
+        while (!pq.empty())
+        {
+            ListNode* node = pq.top(); pq.pop();
+            tail->next = node;
+            tail = tail->next;
+            if (node->next) pq.push(node->next);
         }
-        return res->next;
+
+        return dummy.next;
     }
 };
