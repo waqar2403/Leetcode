52cd4f0a1067732b2a825ba943612781e07a2ce0
Muhammad Waqar <140682775+waqar2403@users.noreply.github.com>
2025-08-29 15:22:55 +0500

    Time: 0 ms (100.00%) | Memory: 9.9 MB (45.71%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        sort(stones.begin(), stones.end());          
-        while (stones.size() > 1) {                  
-            int a = stones.back(); stones.pop_back(); 
-            int b = stones.back(); stones.pop_back(); 
+        sort(stones.begin(),stones.end());
+        while(stones.size()> 1){
+            int a = stones.back(); stones.pop_back();
+            int b = stones.back(); stones.pop_back();
 
-            if (a != b) stones.push_back(a - b);      
-            sort(stones.begin(), stones.end());       
+            if(a != b) stones.push_back(a-b);
+            sort(stones.begin(),stones.end());
         }
-
-        return stones.empty() ? 0 : stones[0];
+        return stones.empty()? 0: stones[0];
     }
 };
