805efe8237c3ed361a5316c03ec7f6fd0cdcc304
Muhammad Waqar <140682775+waqar2403@users.noreply.github.com>
2025-08-29 15:13:23 +0500

    Time: 7 ms (15.95%) | Memory: 19 MB (20.01%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -9,8 +9,9 @@ public:
         vector<pair<int,int>> v;
         for(auto it:mp){
             v.push_back(make_pair(it.second,it.first));
-            sort(v.rbegin(),v.rend());
+            
         }
+        sort(v.rbegin(),v.rend());
         for(int i=0;i<v.size() && k!=0;i++)
         {
             ans.push_back(v[i].second);
